1636663460a51fb831b7bc26d5051b2356b079d5
Veronika <vnika.korolkova@gmail.com>
2022-12-08 19:32:33 +0200

    Added negative checks

--- pages/product_page.py
+++ pages/product_page.py
@@ -1,9 +1,7 @@
 from .base_page import BasePage
 from selenium.webdriver.common.by import By
 from .locators import ProductPageLocators
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-import time
+
 
 class ProductPage(BasePage):
     def add_product_to_basket(self):
@@ -11,10 +9,19 @@ class ProductPage(BasePage):
         add_to_basket_button.click()
         self.solve_quiz_and_get_code()
         self.should_be_book_in_basket()
+    
         
 
     def should_be_book_in_basket(self):
         assert (self.browser.find_element(*ProductPageLocators.BOOK_NAME_IN_BASKET)).text == (self.browser.find_element(*ProductPageLocators.BOOK_NAME)).text, "The name of the book in the basket does not match"
         assert (self.browser.find_element(*ProductPageLocators.BOOK_PRICE)).text == (self.browser.find_element(*ProductPageLocators.BASKET_PRICE)).text, "The price of the book in the basket does not match"
 
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocators.BOOK_NAME_IN_BASKET), \
+           "Success message is presented, but should not be"
+
+    def should_be_disappeared_success_message(self):
+        assert self.is_disappeared(*ProductPageLocators.BOOK_NAME_IN_BASKET), \
+           "Success message is presented, but should be disappeared"
+        
         
--- test_product_page.py
+++ test_product_page.py
@@ -21,3 +21,25 @@ def test_guest_can_add_product_to_basket(browser, link):
     page.open()
     page.add_product_to_basket()
 
+@pytest.mark.xfail
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207/?promo=newYear2019"
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_product_to_basket()
+    page.should_not_be_success_message()
+
+def test_guest_cant_see_success_message(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207/?promo=newYear2019"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_not_be_success_message()
+
+@pytest.mark.xfail
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207/?promo=newYear2019"
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_product_to_basket()
+    page.should_be_disappeared_success_message()
+    
